1302753a3ee9b3f519ac9b92f631b492f85079a5
Melanie Milsted <melanie.milsted@gmail.com>
2018-10-12 04:42:36 +0000

    initial commit

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,5 +1,5 @@
 function sendMail(contactForm) {
-    emailjs.send("gmail", "rosie", {
+    emailjs.send("gmail", "mel", {
         "from_name": contactForm.name.value,
         "from_email": contactForm.emailaddress.value,
         "project_request": contactForm.projectsummary.value
@@ -10,16 +10,17 @@ function sendMail(contactForm) {
         },
         function(error) {
             console.log("FAILED", error);
-        });
+        }
+        );
 }
 
 
- var template_params = {
-   "from_name": "from_name_value",
-   "from_email": "from_email_value",
-   "project_request": "project_request_value"
-};
+ //var template_params = {
+ //  "from_name": "from_name_value",
+ //  "from_email": "from_email_value",
+   //"project_request": "project_request_value"
+//};
 
-var service_id = "default_service";
-var template_id = "rosie";
-emailjs.send(service_id,template_id,template_params);
+//var service_id = "default_service";
+//var template_id = "mel";
+//emailjs.send(service_id,template_id,template_params);
